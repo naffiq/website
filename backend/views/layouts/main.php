89069d4caf9a78cb5ec76bb5ee88d03436c8d050
Galymzhan Abdugalimov <abdu.galymzhan@gmail.com>
2016-11-10 19:19:52 +0600

    Removed home link from admin menu

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -35,7 +35,6 @@ AppAsset::register($this);
         ],
     ]);
     $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
     ];
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
